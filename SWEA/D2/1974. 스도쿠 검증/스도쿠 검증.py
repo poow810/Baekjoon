609638da1427c992a20290541e9dfc8afd809391
poow810 <111109429+poow810@users.noreply.github.com>
2024-02-21 23:38:03 +0900

    [D2] Title: 스도쿠 검증, Time: 109 ms, Memory: 44,532 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,63 +1,42 @@
+def solution():
+
+    # row
+    for i in range(9):
+        lst = [x for x in range(1, 10)]
+        for j in range(9):
+            if mp[j][i] in lst:
+                lst.remove(mp[j][i])
+            else:
+                return 0
+
+
+    # col
+    for i in range(9):
+        lst = [x for x in range(1, 10)]
+        for j in range(9):
+            if mp[i][j] in lst:
+                lst.remove(mp[i][j])
+            else:
+                return 0
+    
+    # square
+    arr = [(0, 0), (0, 3), (0, 6), (3, 0), (3, 3), (3, 6),
+            (6, 0), (6, 3), (6, 6)]
+    for i in arr:
+        x, y = i
+        lst = [x for x in range(1, 10)]
+        for j in range(x, x+3):
+            for k in range(y, y+3):
+                if mp[j][k] in lst:
+                    lst.remove(mp[j][k])
+                else:
+                    return 0
+    return 1
+
+
 T = int(input())
 for t in range(T):
     mp = [list(map(int, input().split())) for _ in range(9)]
+    result = solution()
 
-    def col():
-
-        valid = False
-        for i in range(9):
-            lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            for j in range(9):
-                if mp[j][i] in lst:
-                    lst.remove(mp[j][i])
-                else:
-                    return False
-            
-            if not lst:
-                valid = True
-        
-        return valid
-
-
-    def row():
-
-        valid = False
-        for i in range(9):
-            lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            for j in range(9):
-                if mp[i][j] in lst:
-                    lst.remove(mp[i][j])
-                else:
-                    return False
-                
-            if not lst:
-                valid = True
-            
-        return valid
-    
-
-    def block():
-        
-        check = [(0, 0), (0, 3), (0, 6), (3, 0), (3, 3),
-                 (3, 6), (6, 0), (6, 3), (6, 6)]
-        valid = False
-
-        for k in check:
-            x, y = k
-            lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            for i in range(x, x+3):
-                for j in range(y, y+3):
-                    if mp[j][i] in lst:
-                        lst.remove(mp[j][i])
-                    else:
-                        return False
-                
-            if not lst:
-                valid = True
-
-        return valid
-
-    if col() and row() and block():
-        print(f"#{t+1}", 1)
-    else:
-        print(f"#{t+1}", 0)
+    print(f"#{t+1}", result)
