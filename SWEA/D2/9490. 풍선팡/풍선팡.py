08b7f9e1ffc763571805e857f1029fd17061db45
poow810 <111109429+poow810@users.noreply.github.com>
2024-02-23 21:11:21 +0900

    [D2] Title: 풍선팡, Time: 292 ms, Memory: 61,252 KB -BaekjoonHub

--- SWEA/D2/9490. 풍선팡/풍선팡.py
+++ SWEA/D2/9490. 풍선팡/풍선팡.py
@@ -1,37 +1,35 @@
+def is_valid(nx, ny):
+    return 0 <= nx < N and 0 <= ny < M
+
+def solution():
+
+    max_num = 0
+    for i in range(N):
+        for j in range(M):
+            w = mp[i][j]
+            add = w
+            for z in range(1, w+1):
+                for k in range(4):
+                    nx = i + z*dx[k]
+                    ny = j + z*dy[k]
+
+                    if not is_valid(nx, ny):
+                        continue
+
+                    add += mp[nx][ny]
+
+            if add > max_num:
+                max_num = add
+
+    return max_num         
+                    
+
 T = int(input())
 for t in range(T):
     N, M = map(int, input().split())
     mp = [list(map(int, input().split())) for _ in range(N)]
-    
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
+    ans = solution()
 
-    def is_valid(nx, ny):
-        return 0 <= nx < N and 0 <= ny < M
-
-    def solution(x, y):
-        
-        lst = []
-        for i in range(N):
-            for j in range(M):
-                add = mp[i][j]
-                for z in range(1, mp[i][j] + 1):
-                    for k in range(4):
-                        nx = i + z * dx[k]
-                        ny = j + z * dy[k]
-
-                        if not is_valid(nx, ny):
-                            continue
-                
-                        add += mp[nx][ny]
-                lst.append(add)
-                
-        return lst
-    
-    lst = solution(0, 0)
-    max_num = 0
-    for i in lst:
-        if i >= max_num:
-            max_num = i
-
-    print(f"#{t+1}", max_num)
+    print(f"#{t+1}", ans)
