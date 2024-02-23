b5442bb964a7507d27f73d0f2257c847e1fdd8a3
poow810 <111109429+poow810@users.noreply.github.com>
2024-02-23 21:24:22 +0900

    [D2] Title: 파리 퇴치, Time: 121 ms, Memory: 49,104 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,24 +1,31 @@
 def is_valid(nx, ny):
     return 0 <= nx < N and 0 <= ny < N
 
-def check():
+def solution():
 
-    max_add = 0
-    for i in range(N-M+1):
-        for j in range(N-M+1):
+    max_num = 0
+    for i in range(N-1):
+        for j in range(N-1):
             add = 0
-            for k in range(i, i+M):
-                for z in range(j, j+M):
-                    add += mp[k][z]
-            if add > max_add:
-                max_add = add
-    return max_add
+            for z in range(i, i+M):
+                for k in range(j, j+M):
 
+                    if not is_valid(z, k):
+                        continue
+                    add += mp[z][k]
+
+            if add > max_num:
+                max_num = add
+
+    return max_num         
+                    
 
 T = int(input())
 for t in range(T):
     N, M = map(int, input().split())
     mp = [list(map(int, input().split())) for _ in range(N)]
-    ans = check()
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    ans = solution()
 
     print(f"#{t+1}", ans)
