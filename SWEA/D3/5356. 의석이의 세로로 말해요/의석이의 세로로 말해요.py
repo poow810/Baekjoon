eb42715a127e35215af855a296d17fd337f34908
poow810 <111109429+poow810@users.noreply.github.com>
2024-03-02 21:57:01 +0900

    [D3] Title: 의석이의 세로로 말해요, Time: 194 ms, Memory: 61,724 KB -BaekjoonHub

--- SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.py
+++ SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.py
@@ -1,17 +1,18 @@
+from collections import deque
+
 T = int(input())
 for t in range(T):
-
-    lst = [list(input()) for _ in range(5)]
+ 
+    lst = [deque(input()) for _ in range(5)]
     max_len = 0
     for i in lst:
         if len(i) > max_len:
             max_len = len(i)
-    ans = []
     a = ""
     for i in range(max_len):
         for j in range(5):
             if not lst[j]:
                 continue
-            a += lst[j].pop(0)
-
+            a += lst[j].popleft()
+ 
     print(f"#{t+1}", a)
