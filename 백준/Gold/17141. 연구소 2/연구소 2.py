382cc5e89711b66ee2214eeccccd19106959d8a2
poow810 <111109429+poow810@users.noreply.github.com>
2024-03-17 21:41:05 +0900

    [Gold IV] Title: 연구소 2, Time: 816 ms, Memory: 34180 KB -BaekjoonHub

---
+++ 백준/Gold/17141. 연구소 2/연구소 2.py
@@ -0,0 +1,70 @@
+import sys, copy
+from collections import deque
+from itertools import combinations
+
+
+def is_valid(nx, ny):
+    return 0 <= nx < N and 0 <= ny < N
+
+
+def bfs(np, check):
+    
+    max_count = 0
+    queue = deque()
+    for a, b in check:
+        np[a][b] = 1
+        queue.append((a, b))
+
+    while queue:
+        x, y = queue.popleft()
+        max_count = max(max_count, np[x][y])
+
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+
+            if not is_valid(nx, ny):
+                continue
+
+            if np[nx][ny] == 0:
+                np[nx][ny] = np[x][y] + 1
+                queue.append((nx, ny))
+    
+    for z in range(N):
+        for g in range(N):
+            if np[z][g] == 0:
+                return -1
+    
+    return max_count-1
+    
+
+
+N, M = map(int, sys.stdin.readline().split())
+mp = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+virus = []
+for i in range(N):
+    for j in range(N):
+        if mp[i][j] == 1:
+            mp[i][j] = '-'
+        elif mp[i][j] == 2:
+            virus.append((i, j))
+
+virus_lst = list(combinations(virus, M))
+ans = set()
+for v in virus_lst:
+    np = copy.deepcopy(mp)
+    check = []
+    for x, y in virus:
+        np[x][y] = 0
+    for c_x, c_y in v:
+        check.append((c_x, c_y))
+    ans.add(bfs(np, check))
+
+if len(ans) == 1 and -1 in ans:
+    print(-1)
+else:
+    if -1 in ans:
+        ans.remove(-1)
+    print(min(ans))
