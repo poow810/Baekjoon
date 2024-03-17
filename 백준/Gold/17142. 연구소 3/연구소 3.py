3881b32eee8277b2dfe09d3ac3a429b62cd3f914
poow810 <111109429+poow810@users.noreply.github.com>
2024-03-17 21:57:13 +0900

    [Gold III] Title: 연구소 3, Time: 412 ms, Memory: 118040 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -1,91 +1,82 @@
-# 가장 처임에 모든 바이러스는 비활성 상태이고,
-# 활성 상태인 바이러슨 상하좌우 인접한 모든 빈칸으로 동시
-# 복제되며, 1초가 걸린다. 연구소의 바이러스 m개를 활성 상태로
-# 연구소는 빈 칸, 벽, 바이러스로 이루어져 있으며,
-# 벽은 한 칸을 가득 차지한다. 활성 바이러스가 비활성 바이러스
-# 가 있는 칸으로 가면 비활성 바이러스는 활성 바이러스
-
+import sys, copy
 from collections import deque
 from itertools import combinations
 
-dirt_y = [-1, 1, 0, 0]
-dirt_x = [0, 0, -1, 1]
 
-min_time = 21e8
-
-n, m = map(int, input().split())
-my_map = [list(map(int, input().split())) for _ in range(n)]
+def is_valid(nx, ny):
+    return 0 <= nx < N and 0 <= ny < N
 
 
-def bfs():
-    global my_flag
-    while my_q:
-        now_y, now_x, now_time = my_q.popleft()
-        # if now_time-1 >= min_time:
-        #     my_flag = 1
-        #     return
+def bfs(np, check, check_zero):
+    
+    max_count = 0
+    queue = deque()
+    for a, b in check:
+        np[a][b] = 1
+        queue.append((a, b))
 
-        for i in range(4):
-            new_y = now_y + dirt_y[i]
-            new_x = now_x + dirt_x[i]
-            new_time = now_time + 1
-            if new_y > n - 1 or new_y < 0 or new_x > n - 1 or new_x < 0:
+    while queue:
+        x, y = queue.popleft()
+        max_count = max(max_count, np[x][y])
+
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+
+            if not is_valid(nx, ny):
                 continue
-            if my_new_map[new_y][new_x] == -1:
-                continue
-            if visited[new_y][new_x] == 1:
-                continue
-            if my_new_map[new_y][new_x] != 0 and my_new_map[new_y][new_x] != -2:
-                my_new_map[new_y][new_x] = min(my_new_map[new_y][new_x], new_time)
-                continue
-            visited[new_y][new_x] =1
-            my_new_map[new_y][new_x] = new_time
-            my_q.append([new_y, new_x, new_time])
 
+            if np[nx][ny] == 0:
+                check_zero -= 1              
+                np[nx][ny] = np[x][y] + 1
+                queue.append((nx, ny))
 
-virus_coords = []
-for i in range(n):
-    for j in range(n):
-        if my_map[i][j] == 2:
-            virus_coords.append([i, j])
-
-my_comb_virus = list(combinations(virus_coords, m))
-
-for my_virus in my_comb_virus:
-    my_flag = 0
-    my_q = deque()
-    my_new_map = [[0 for _ in range(n)] for _ in range(n)]
-    visited = [[0 for _ in range(n)] for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if my_map[i][j] == 2 and [i, j] not in my_virus:
-                my_new_map[i][j] = -2
-            if my_map[i][j] == 1:
-                my_new_map[i][j] = -1
-    for j in my_virus:
-        my_q.append([j[0], j[1], 1])
-        my_new_map[j[0]][j[1]] = 1
-        visited[j[0]][j[1]] = 1
-    bfs()
-
-    if my_flag == 1:
-        continue
-    else:
-        my_new_flag = 0
-        my_max = 0
-        for i in range(n):
-            for j in range(n):
-                if my_new_map[i][j] == 0:
-                    my_new_flag = 1
+            if np[nx][ny] == '*':
+                if check_zero == 0:
                     break
-                if my_new_map[i][j] - 1 > my_max and my_map[i][j] != 2:
-                    my_max = my_new_map[i][j] - 1
-            if my_new_flag == 1:
-                break
-        if not my_new_flag:
-            min_time = min(my_max, min_time)
+                np[nx][ny] = np[x][y] + 1
+                queue.append((nx, ny))
 
-if min_time == 21e8:
+
+    for z in range(N):
+        for g in range(N):
+            if np[z][g] == 0:
+                return -1
+    
+    return max_count-1
+    
+
+
+N, M = map(int, sys.stdin.readline().split())
+mp = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+virus = []
+zero_count = 0
+for i in range(N):
+    for j in range(N):
+        if mp[i][j] == 1:
+            mp[i][j] = '-'
+        elif mp[i][j] == 2:
+            virus.append((i, j))
+        else:
+            zero_count += 1
+
+virus_lst = list(combinations(virus, M))
+ans = set()
+for v in virus_lst:
+    np = copy.deepcopy(mp)
+    check = []
+    for x, y in virus:
+        np[x][y] = '*'
+    for c_x, c_y in v:
+        mp[c_x][c_y] = 0
+        check.append((c_x, c_y))
+    ans.add(bfs(np, check, zero_count))
+
+if len(ans) == 1 and -1 in ans:
     print(-1)
 else:
-    print(min_time)
+    if -1 in ans:
+        ans.remove(-1)
+    print(min(ans))
