f8795e7aa068af3c5974bea2384cc1251c1892bb
poow810 <111109429+poow810@users.noreply.github.com>
2024-04-03 23:29:02 +0900

    [D4] Title: 정사각형 방, Time: 1,045 ms, Memory: 51,676 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.py
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -3,50 +3,45 @@ from collections import deque
 def is_valid(nx, ny):
     return 0 <= nx < N and 0 <= ny < N
 
-def bfs():
 
-    ans = []
+def bfs(x, y):
+
     queue = deque()
-    for i in range(N):
-        for j in range(N):
-            count = 1
-            if is_valid(i-1, j) and mp[i-1][j] == mp[i][j]+1 or is_valid(i+1, j) and mp[i+1][j] == mp[i][j]+1 or is_valid(i, j-1) and mp[i][j-1] == mp[i][j]+1 or is_valid(i, j+1) and mp[i][j+1] == mp[i][j]+1:
-                queue.append((i, j))
+    queue.append((x, y))
+    count = 1
 
-                while queue:
-                    x, y = queue.popleft()
+    while queue:
+        x, y = queue.popleft()
 
-                    for k in range(4):
-                        nx = x + dx[k]
-                        ny = y + dy[k]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-                        if not is_valid(nx, ny):
-                            continue
-
-                        if mp[x][y]+1 == mp[nx][ny]:
-                            queue.append((nx, ny))
-                            count += 1
-                ans.append((mp[i][j], count))
-                                        
-    return ans
-
+            if not is_valid(nx, ny):
+                continue
+            
+            if mp[nx][ny] == mp[x][y] + 1:
+                count += 1
+                queue.append((nx, ny))
+    return count
+            
 
 T = int(input())
 for t in range(T):
     N = int(input())
     mp = [list(map(int, input().split())) for _ in range(N)]
-    visited = [[0]*N for _ in range(N)]
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
+    lst = [0, 0]
+    max_check = 0
+    for i in range(N):
+        for j in range(N):
+            count = bfs(i, j)
+            if count > lst[1]:
+                lst[1] = count
+                lst[0] = mp[i][j]
+            elif count == lst[1]:
+                if mp[i][j] < lst[0]:
+                    lst[0] = mp[i][j]
     
-    result = bfs()
-    result.sort()
-    max_count = 0
-    for i in result:
-        if max_count < i[1]:
-            max_count = i[1]
-    for j in result:
-        if j[1] == max_count:
-            print(f"#{t+1}", j[0], j[1])
-            break
-    
+    print(f"#{t+1}", lst[0], lst[1])
