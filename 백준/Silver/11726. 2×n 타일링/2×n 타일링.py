f85fcd407bd2c829608cd046da7d4a11f6099544
poow810 <111109429+poow810@users.noreply.github.com>
2024-04-04 16:25:13 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,13 @@
+import sys
+
+
+N = int(sys.stdin.readline().rstrip())
+dp = [0] * (1001)
+dp[1] = 1
+dp[2] = 2
+dp[3] = 3
+
+for i in range(4, 1001):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+print(dp[N]%10007)
