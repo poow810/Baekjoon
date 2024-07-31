09a2a08d132d3a2b6b4bb03ef55d6e408dba2561
poow810 <111109429+poow810@users.noreply.github.com>
2024-08-01 08:49:49 +0900

    [Silver I] Title: 미로 탐색, Time: 148 ms, Memory: 114644 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -5,31 +5,32 @@ from collections import deque
 def is_valid(nx, ny):
     return 0 <= nx < N and 0 <= ny < M
 
-def bfs(x, y):
 
+def bfs(x, y):
+    global ans
+    
     queue = deque()
     queue.append((x, y, 1))
-    visited = [[0] * M for _ in range(N)]
-    visited[x][y] = 1
-
+    mp[x][y] = 0
     while queue:
         x, y, count = queue.popleft()
         if (x, y) == (N-1, M-1):
             return count
+        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if not is_valid(nx, ny) or visited[nx][ny] == 1 or mp[nx][ny] == '0':
+            if not is_valid(nx, ny):
                 continue
             
             if mp[nx][ny] == '1':
-                visited[nx][ny] = 1
-                queue.append((nx, ny, count+1))
-
-
+                mp[nx][ny] = 0
+                queue.append((nx, ny, count + 1))
+            
+    
 N, M = map(int, sys.stdin.readline().split())
 mp = [list(map(str, sys.stdin.readline().strip())) for _ in range(N)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-print(bfs(0, 0))
+print(bfs(0,0))
