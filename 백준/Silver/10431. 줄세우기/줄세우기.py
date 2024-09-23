1714ccde72e2ad1ac00bba32669e84266103b30c
poow810 <111109429+poow810@users.noreply.github.com>
2024-09-24 00:03:55 +0900

    [Silver V] Title: 줄세우기, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.py
+++ 백준/Silver/10431. 줄세우기/줄세우기.py
@@ -1,14 +1,14 @@
 import sys
 
 P = int(sys.stdin.readline())
-for i in range(P):
+for _ in range(P):
     T, *lst = map(int, sys.stdin.readline().split())
-    step = 0
+    count = 0
 
-    # 버블 정렬 이용하여 앞에꺼부터 하나씩 뒤에꺼랑 자리 바꿔주면 됨
-    for j in range(len(lst)-1):
-        for k in range(j+1, len(lst)):
-            if lst[j] > lst[k]:
-                lst[j], lst[k] = lst[k], lst[j]
-                step += 1
-    print(T, step)
+    for i in range(len(lst)-1):
+        for j in range(i+1, len(lst)):
+            if lst[i] > lst[j]:
+                lst[i], lst[j] = lst[j], lst[i]
+                count += 1
+
+    print(T, count)
