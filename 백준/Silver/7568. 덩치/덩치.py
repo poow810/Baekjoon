b8345cc2f11cebca3d0b7300de2972d91861009d
poow810 <111109429+poow810@users.noreply.github.com>
2024-09-24 22:57:54 +0900

    [Silver V] Title: 덩치, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,17 +1,18 @@
-n = int(input())
- 
-data = [] # 입력받은 정보를 저장할 리스트 data
-ans = [] # 등수정보를 저장할 리스트 ans
-for i in range(n):
-    a, b = map(int, input().split())
-    data.append((a, b)) # 몸무계와 키를 묶어서 append 해줌
- 
-for i in range(n):
+import sys
+
+N = int(sys.stdin.readline())
+lst = []
+ans = []
+for _ in range(N):
+    x, y = map(int, sys.stdin.readline().split())
+    lst.append((x, y))
+
+for i in range(N):
     count = 0
-    for j in range(n):
-        if data[i][0] < data[j][0] and data[i][1] < data[j][1]: # 몸무게와 키 모두 자신보다 큰 사람의 수를 센다
-            count += 1 
-    ans.append(count + 1) # 덩치 등수는 자신보다 몸무계 키 모두 큰 사람의 수 + 1 이므로 count + 1을 ans에 append한다.
- 
-for d in ans:
-    print(d,end=" ")
+    for j in range(N):
+        if lst[i][0] < lst[j][0] and lst[i][1] < lst[j][1]:
+            count += 1
+    ans.append(count + 1)
+
+for j in ans:
+    print(j, end=" ")
