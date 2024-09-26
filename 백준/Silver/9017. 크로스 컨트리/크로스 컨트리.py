a523b1a7a60e0d4054639c3c0b774673d97a5526
poow810 <111109429+poow810@users.noreply.github.com>
2024-09-26 22:05:02 +0900

    [Silver III] Title: 크로스 컨트리, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
@@ -1,41 +1,42 @@
 import sys
- 
-T = int(sys.stdin.readline())
 
-for t in range(T):
-    dic = dict()
+T = int(sys.stdin.readline())
+for _ in range(T):
     N = int(sys.stdin.readline())
     lst = list(map(int, sys.stdin.readline().split()))
+    max_num = max(lst)
+    team = {}
+    check = [[0, 0, 0, 0] for _ in range(max_num + 1)]
 
-    for i in lst:
-        if i in dic:
-            dic[i] += 1
+    for i in range(N):
+        if lst[i] not in team:
+            team[lst[i]] = 1
         else:
-            dic[i] = 1
-    
-    to_remove = []
-    for key in dic:
-        if dic[key] < 6:
-            to_remove.append(key)
-    for key in to_remove:
-        del dic[key]
+            team[lst[i]] += 1
+            if team[lst[i]] == 5:
+                check[lst[i]][1] = i
+                check[lst[i]][3] = lst[i]
+
     idx = 1
-    score = dict()
-    for j in range(len(lst)):
-        if lst[j] not in dic:
-            continue
-
-        if lst[j] not in score:
-            score[lst[j]] = [1, idx, 0]
+    c = 0
+    while c < N:
+        if team[lst[c]] >= 6:
+            if check[lst[c]][2] < 4:
+                check[lst[c]][2] += 1
+                check[lst[c]][0] += idx
+            idx += 1
+        
+        c += 1
+    
+    idx = 0
+    while True:
+        if check[idx][0] == 0:
+            del check[idx]
         else:
-            if score[lst[j]][0] < 4:
-                score[lst[j]][0] += 1
-                score[lst[j]][1] += idx
-            elif score[lst[j]][0] == 4:
-                score[lst[j]][0] += 1
-                score[lst[j]][2] += idx
+            idx += 1
+        if idx == len(check):
+            break
+    check.sort(key=lambda x:(x[0], x[1]))
+    print(check[0][3])
     
-        idx += 1
     
-    score = sorted(score.items(), key=lambda x: (x[1][1], x[1][2]))
-    print(score[0][0])
