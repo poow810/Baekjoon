a6996d8ff555a4e8d53a76fb61b0faa47f2d157b
poow810 <111109429+poow810@users.noreply.github.com>
2024-09-27 22:09:37 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 324 ms, Memory: 62812 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,17 +1,15 @@
 import sys
- 
+
 N, M = map(int, sys.stdin.readline().split())
-dic = dict()
-for i in range(N):
+dic = {}
+for _ in range(N):
     word = sys.stdin.readline().strip()
-    if len(word) < M:
-        continue
-    
     if word not in dic:
-        dic[word] = [1, len(word)]
+        dic[word] = [1, len(word), word]
     else:
         dic[word][0] += 1
-
-answer = sorted(dic.items(), key=lambda x: (-x[1][0], -x[1][1], x[0]), reverse=False)
-for i in answer:
-    print(i[0])
+lst = list(dic.values())
+lst.sort(key = lambda x: (-x[0], -x[1], x[2]))
+for i in lst:
+    if len(i[2]) >= M:
+        print(i[2])
