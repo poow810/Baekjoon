d5aef8779473c9d22552ab65c15413def542e119
poow810 <111109429+poow810@users.noreply.github.com>
2024-09-28 00:03:42 +0900

    [Silver III] Title: 햄버거 분배, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -1,16 +1,15 @@
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
-lst = list(sys.stdin.readline().strip())
-check = [0] * N
+string = list(sys.stdin.readline().strip())
+check = [0]*(N)
 count = 0
 
 for i in range(N):
-    if lst[i] == 'P':
+    if string[i] == 'P':
         for j in range(max(0, i-K), min(N, i+K+1)):
-            if lst[j] == 'H' and check[j] == 0:
-                check[j] = 1
+            if string[j] == 'H':
+                string[j] = 0
                 count += 1
                 break
-
 print(count)
