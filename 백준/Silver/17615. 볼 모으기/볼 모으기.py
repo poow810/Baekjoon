2a05507d00293d56162a6d4ef466618e7aa44dc6
poow810 <111109429+poow810@users.noreply.github.com>
2024-09-30 01:27:22 +0900

    [Silver I] Title: 볼 모으기, Time: 412 ms, Memory: 32100 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.py
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.py
@@ -3,9 +3,9 @@ import sys
 N = int(sys.stdin.readline())
 string = sys.stdin.readline().strip()
 
-A, B = 0, 0
+A, B, C, D = 0, 0, 0, 0
 
-status_a, status_b = 0, 0
+status_a, status_b, status_c, status_d = 0, 0, 0, 0
 
 for i in range(N-1, -1, -1):
     if string[i] == 'B':
@@ -21,4 +21,18 @@ for j in range(N-1, -1, -1):
     if string[j] == 'B' and status_b == 1:
         B += 1
 
-print(min(A, B))
+for k in range(N):
+    if string[k] == 'R':
+        status_c = 1
+    
+    if string[k] == 'B' and status_c == 1:
+        C += 1
+
+for z in range(N):
+    if string[z] == 'B':
+        status_d = 1
+
+    if string[z] == 'R' and status_d == 1:
+        D += 1
+
+print(min(A, B, C, D))
