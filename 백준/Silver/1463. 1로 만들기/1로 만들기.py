e6094150a90d1233e5a2194f86bb35b3989ee331
poow810 <111109429+poow810@users.noreply.github.com>
2024-09-29 18:30:08 +0900

    [Silver III] Title: 1로 만들기, Time: 476 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,26 +1,14 @@
 import sys
 
-
-def dfs(num, count):
-    global min_count
-
-    if num == 1:
-        min_count = min(min_count, count)
-        return
-    
-    if count > min_count:
-        return
-
-    if num % 3 == 0:
-        dfs(num // 3, count + 1)
-
-    if num % 2 == 0:
-        dfs(num // 2, count + 1)
-
-    dfs(num - 1, count + 1)    
+N = int(sys.stdin.readline().strip())
+dp = [0] * (N+1)
 
 
-N = int(sys.stdin.readline())
-min_count = 1e99
-dfs(N, 0)
-print(min_count)
+for i in range(2, N+1):
+    dp[i] = dp[i-1] + 1
+    if i % 3 == 0:
+        dp[i] = min(dp[i//3]+1, dp[i])
+    if i % 2 == 0:
+        dp[i] = min(dp[i//2]+1, dp[i])
+
+print(dp[N])
