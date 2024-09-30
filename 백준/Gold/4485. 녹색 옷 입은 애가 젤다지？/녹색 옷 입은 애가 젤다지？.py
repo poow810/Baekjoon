fd82f56c2c7d578657d0b9c148a50cefc3042942
poow810 <111109429+poow810@users.noreply.github.com>
2024-10-01 08:11:11 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 96 ms, Memory: 34212 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,46 +1,53 @@
 import sys
 from heapq import heappop, heappush
 
-
 def is_valid(nx, ny):
-    return 0 <= nx < N and 0 <= ny < N
-
+		return 0 <= nx < N and 0 <= ny < N
+		
 
 def solution(x, y):
-
+		
     hq = []
-    heappush(hq, (mp[0][0], x, y))
-    visited[x][y] = mp[x][y]
-
+    heappush(hq, (mp[x][y], x, y))
+    distance[x][y] = mp[x][y]
+    
     while hq:
-        rupy, x, y = heappop(hq)
-
-        if visited[x][y] < rupy:
-            continue 
+        cost, x, y = heappop(hq)
         
-        visited[x][y] = rupy
+        if cost > distance[x][y]:
+            continue
+        
+        distance[x][y] = cost
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-
+    
             if not is_valid(nx, ny):
                 continue
+                    
+            next_dist = mp[nx][ny]
+            new_dist = next_dist + cost
+            
+            if new_dist < distance[nx][ny]:
+                distance[nx][ny] = new_dist
+                heappush(hq, (new_dist, nx, ny))
 
-            new_rupy = rupy + mp[nx][ny]
-            if new_rupy >= visited[nx][ny]:
-                continue
-            visited[nx][ny] = new_rupy
-            heappush(hq, (new_rupy, nx, ny))
+
 
 count = 0
 while True:
     count += 1
-    N = int(sys.stdin.readline())
+    N = int(sys.stdin.readline().strip())
+    
     if N == 0:
-        exit()
+        break
+
     mp = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-    visited = [[int(1e99)]*N for _ in range(N)]
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
+
+    distance = [[int(1e99) for _ in range(N)] for _ in range(N)]
     solution(0, 0)
-    print(f"Problem {count}: {visited[N-1][N-1]}")
+
+    print(f"Problem {count}: {distance[N-1][N-1]}")
