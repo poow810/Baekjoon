452b86c0435251d517eed912cf0031275e7b9b64
poow810 <111109429+poow810@users.noreply.github.com>
2024-09-30 16:17:51 +0900

    [Silver II] Title: KCPC, Time: 116 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3758. KCPC/KCPC.py
+++ 백준/Silver/3758. KCPC/KCPC.py
@@ -1,23 +1,25 @@
 import sys
 
-N = int(sys.stdin.readline())
-for _ in range(N):
+T = int(sys.stdin.readline().strip())
+for _ in range(T):
     n, k, t, m = map(int, sys.stdin.readline().split())
     lst = [[0 for _ in range(k)] for _ in range(n)]
     submit = [0 for _ in range(n)]
     time = [0 for _ in range(n)]
-    for g in range(m):
+    
+    for z in range(m):
         i, j, s = map(int, sys.stdin.readline().split())
         lst[i-1][j-1] = max(lst[i-1][j-1], s)
-        time[i-1] = g
         submit[i-1] += 1
-    
+        time[i-1] = z
+
     ans = []
-    for idx in range(len(lst)):
-        ans.append([sum(lst[idx]), submit[idx], time[idx], idx])
-    
-    ans.sort(key=lambda x: (-x[0], x[1], x[2]))
+    for k in range(len(lst)):
+        ans.append([sum(lst[k]), submit[k], time[k], k+1])
+
+    ans.sort(key=lambda x : (-x[0], x[1], x[2]))
+
     for i in range(len(ans)):
-        if ans[i][3] == t-1:
+        if ans[i][3] == t:
             print(i+1)
-            break
+
