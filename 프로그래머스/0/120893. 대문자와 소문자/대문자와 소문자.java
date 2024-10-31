f213db339a4cd1f0e883e299bfddb67c5fcaef9b
poow810 <111109429+poow810@users.noreply.github.com>
2024-10-31 22:48:53 +0900

    [level 0] Title: 대문자와 소문자, Time: 1.21 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < my_string.length(); i++) {
+            if (Character.isUpperCase(my_string.charAt(i))) {
+                answer.append(Character.toLowerCase(my_string.charAt(i)));
+            } else {
+                answer.append(Character.toUpperCase(my_string.charAt(i)));
+            }
+        }
+        return answer.toString();
+    }
+}
