5b4b42eeac0088cf643738173cd34a66897cadbf
poow810 <111109429+poow810@users.noreply.github.com>
2025-01-20 13:54:39 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 35408 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,45 +1,45 @@
 import sys
 from collections import deque
 
-
-def dfs(start):
-    
-    visited1[start] = 1
-    print(start, end = " ")
-
-    for i in graph[start]:
-        if visited1[i] == 0:
-            dfs(i)
-
-
 def bfs(start):
-
+    
     queue = deque()
+    visited = [0] * (N+1)
     queue.append(start)
-    visited2[start] = 1
 
     while queue:
         node = queue.popleft()
-        print(node, end = " ")
+        visited[node] = 1
+        print(node, end=" ")
+
         for i in graph[node]:
-            if visited2[i] == 0:
-                visited2[i] = 1
+            if not visited[i]:
+                visited[i] = 1
                 queue.append(i)
+
+
+def dfs(start):
+
+    visited_dfs[start] = 1
+    print(start, end=" ")
+
+    for i in graph[start]:
+        if visited_dfs[i] == 0:
+            dfs(i)
 
 
 N, M, V = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(N+1)]
 for _ in range(M):
-    a, b = map(int, sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+visited_dfs = [0] * (N+1)
 
 for i in graph:
     i.sort()
 
-visited1 = [0] * (N+1)
 dfs(V)
 print()
-
-visited2 = [0] * (N+1)
 bfs(V)
