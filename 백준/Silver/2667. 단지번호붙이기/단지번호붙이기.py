bedd8ca7c2247d3bc22565c16369a63308b18533
poow810 <111109429+poow810@users.noreply.github.com>
2025-01-22 14:28:49 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -4,42 +4,51 @@ from collections import deque
 def is_valid(nx, ny):
     return 0 <= nx < N and 0 <= ny < N
 
+
 def bfs(x, y):
 
-    queue = deque()
-    queue.append((x, y))
-    mp[x][y] = 0
+    queue = deque([(x, y)])
+    visited[x][y] = 1
+
     count = 1
 
     while queue:
         x, y = queue.popleft()
-        
-        if visited[x][y] == 1:
-            continue
-        
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
 
-            if is_valid(nx, ny) and visited[nx][ny] == 0 and mp[nx][ny] == 1:
-                mp[nx][ny] = 0
-                count += 1
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+
+            if not is_valid(nx, ny):
+                continue
+
+            if mp[nx][ny] != '0' and visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+
+                if mp[nx][ny] != '0':
+                    count += 1
                 queue.append((nx, ny))
-
+    
     return count
 
 
-N = int(sys.stdin.readline())
-mp = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
-visited = [[0] * (N) for _ in range(N)]
+N = int(sys.stdin.readline().strip())
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+visited = [[0] * N for _ in range(N)]
+mp = []
 lst = []
 for i in range(N):
+    mp.append(list(map(str, sys.stdin.readline().strip())))
+
+answer = 0
+for i in range(N):
     for j in range(N):
-        if mp[i][j] == 1:
+        if visited[i][j] == 0 and mp[i][j] != '0':
             lst.append(bfs(i, j))
+            answer += 1
+
+print(answer)
 lst.sort()
-print(len(lst))
 for i in lst:
     print(i)
