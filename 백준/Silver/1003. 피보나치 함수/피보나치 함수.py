3ce9f847ee58577f0d6c917a4ee1ad9a2f1a9bce
poow810 <111109429+poow810@users.noreply.github.com>
2025-02-17 22:57:06 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,18 +1,25 @@
 import sys
 
+def dfs(n):
 
-def fibo(n):
-    l = len(zero)
-    if n >= l:
-        for i in range(l, n+1):
-            zero.append(zero[i-1] + zero[i-2])
-            one.append(one[i-1] + one[i-2])
-    print(zero[n], one[n])
+    zero[0], one[0] = 1, 0
+    zero[1], one[1] = 0, 1
+
+    for i in range(2, n+1):
+        zero[i] = zero[i-1] + zero[i-2]
+        one[i] = one[i-1] + one[i-2]
 
 
-T = int(sys.stdin.readline().rstrip())
-one = [0, 1, 1]
-zero = [1, 0, 1]
+T = int(sys.stdin.readline().strip())
 
 for _ in range(T):
-    fibo(int(sys.stdin.readline().rstrip()))
+    N = int(sys.stdin.readline().strip())
+    zero = [0] * (N+1)
+    one = [0] * (N+1)
+    if N == 0:
+        print(1, 0)
+    elif N == 1:
+        print(0, 1)
+    else:
+        dfs(N)
+        print(zero[N], one[N])
