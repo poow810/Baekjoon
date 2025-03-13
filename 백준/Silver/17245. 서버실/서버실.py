29f2eb26e3a7b72d33d8295d2586c2dcaebeb51e
poow810 <111109429+poow810@users.noreply.github.com>
2025-03-13 13:23:29 +0900

    [Silver II] Title: 서버실, Time: 3480 ms, Memory: 80000 KB -BaekjoonHub

--- 백준/Silver/17245. 서버실/서버실.py
+++ 백준/Silver/17245. 서버실/서버실.py
@@ -1,32 +1,24 @@
 import sys
 
 N = int(sys.stdin.readline().strip())
+
 mp = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-start = 0
-total = 0
-end = 0
-answer = 0
+mp = [num for row in mp for num in row]
 
-for i in mp:
-    if max(i) > end:
-        end = max(i)
-    total += sum(i)
+start = 0
+total = sum(mp)
+end = max(mp)
+answer = 0
 
 while start <= end:
     mid = (start + end) // 2
     count = 0
 
     for i in mp:
-        for j in i:
-            if j > 0:
-                if j > mid:
-                    count += mid
-                else:
-                    count += j
-
+        count += i if i < mid else mid
     
-    if count >= total / 2:
+    if count >= (total / 2):
         end = mid - 1
         answer = mid
     
